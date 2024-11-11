cb463a9d361bb261ca13d6cd69190f72d0471384
musicalmoods <musicalmoods@gmail.com>
2024-11-11 13:54:51 -0800

    testing out manim for marketing

---
+++ teaser_anim.py
@@ -0,0 +1,15 @@
+from manim import *
+from PIL import Image
+
+class SeedlingAnimation(Scene):
+    def construct(self):
+        bg = Rectangle(width=16, height=9).set_fill(BLACK, opacity=1)
+        self.add(bg)
+        seedling_img = Image.open("X:\\Musical_Moods\\Coding\\Inspire_Case_Competiton\\assets\\InspireSeedlingNegative.png")
+        seedling_mobject = ImageMobject(seedling_img).scale(0.5).move_to(ORIGIN)
+        self.play(FadeIn(seedling_mobject))
+        seedling_path = Line(ORIGIN, 4*LEFT + 3*UP)
+        self.play(MoveAlongPath(seedling_mobject, seedling_path), run_time=5, rate_func=smooth)
+        inspire_text = Text("Inspire", font_size=72, color=WHITE, font="Ubuntu").move_to(ORIGIN)
+        self.play(Write(inspire_text))
+        self.wait(2)
