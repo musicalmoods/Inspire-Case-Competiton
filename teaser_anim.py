ab38bcc42662581ed18bc13a3aee99a3b9722a99
musicalmoods <musicalmoods@gmail.com>
2024-11-16 22:41:34 -0800

    deleted useless clips

--- teaser_anim.py
+++ teaser_anim.py
@@ -1,15 +1,14 @@
 from manim import *
-from PIL import Image
 
 class SeedlingAnimation(Scene):
     def construct(self):
         bg = Rectangle(width=16, height=9).set_fill(BLACK, opacity=1)
         self.add(bg)
-        seedling_img = Image.open("X:\\Musical_Moods\\Coding\\Inspire_Case_Competiton\\assets\\InspireSeedlingNegative.png")
-        seedling_mobject = ImageMobject(seedling_img).scale(0.5).move_to(ORIGIN)
-        self.play(FadeIn(seedling_mobject))
+        seedling_mobject = ImageMobject("X:\\Musical_Moods\\Coding\\Inspire_Case_Competiton\\assets\\InspireSeedlingNegative.png").scale(0.5).move_to(ORIGIN)
         seedling_path = Line(ORIGIN, 4*LEFT + 3*UP)
-        self.play(MoveAlongPath(seedling_mobject, seedling_path), run_time=5, rate_func=smooth)
         inspire_text = Text("Inspire", font_size=72, color=WHITE, font="roboto").move_to(ORIGIN)
-        self.play(Write(inspire_text))
-        self.wait(2)
+        self.play(Write(inspire_text), runtime=3)
+        self.play(FadeOut(inspire_text))
+        self.play(FadeIn(seedling_mobject))
+        self.play(MoveAlongPath(seedling_mobject, seedling_path), run_time=5, rate_func=smooth)
+        self.play(FadeOut(seedling_mobject))
