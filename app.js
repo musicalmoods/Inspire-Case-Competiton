52a9e3405ccc74ef8cdbec6ab46592cbceae95db
musicalmoods <musicalmoods@gmail.com>
2024-11-20 19:05:47 -0800

    removed animations b/c made it laggy

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add("show");
-        } else {
-            entry.target.classList.remove("show");
-        }
+document.addEventListener("DOMContentLoaded", () => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            } else {
+                entry.target.classList.remove("show");
+            }
+        });
     });
-});
 
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => observer.observe(el));
+    const hiddenElements = document.querySelectorAll(".hidden");
+    hiddenElements.forEach((el) => observer.observe(el));
+});
 
 function updateSun() {
     const scrollY = window.scrollY;
